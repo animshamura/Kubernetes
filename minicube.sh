b1c44b6668d8c42ddbf8e28274aa79194ebb5ba3
Shamura Ahmad Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-10-02 00:32:06 +0600

    Create minicube.sh

---
+++ minicube.sh
@@ -0,0 +1,11 @@
+#minicube download 
+
+New-Item -Path 'c:\' -Name 'minikube' -ItemType Directory -Force
+Invoke-WebRequest -OutFile 'c:\minikube\minikube.exe' -Uri 'https://github.com/kubernetes/minikube/releases/latest/download/minikube-windows-amd64.exe' -UseBasicParsing'
+
+#minicube install 
+
+$oldPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine)
+if ($oldPath.Split(';') -inotcontains 'C:\minikube'){
+  [Environment]::SetEnvironmentVariable('Path', $('{0};C:\minikube' -f $oldPath), [EnvironmentVariableTarget]::Machine)
+}
